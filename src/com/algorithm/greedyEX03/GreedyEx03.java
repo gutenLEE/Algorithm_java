b62c762baf2c2ef8b84d91ff1b1aaeedb7b2f39d
lee <3bnm1128@naver.com>
2021-02-17 22:11:14 +0900

    1이 될 때까지

---
+++ src/com/algorithm/greedyEX03/GreedyEx03.java
@@ -0,0 +1,30 @@
+package com.algorithm.greedyEX03;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class GreedyEx03 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        int cnt = 0;
+
+        while (N != 1){
+            if(N % K == 0){
+               N /= K;
+               cnt++;
+            }
+            else{
+                N -= 1;
+                cnt++;
+            }
+
+        }
+        System.out.println(cnt);
+    }
+}
