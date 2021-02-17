75f3b0808a8ff1e79606a379f4e83d4eb87f9e07
lee <3bnm1128@naver.com>
2021-02-17 23:01:31 +0900

    백준, 그리디 - 11399번 ATM

---
+++ src/com/algorithm/greedyEx04/GreedyEx04.java
@@ -0,0 +1,30 @@
+package com.algorithm.greedyEx04;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class GreedyEx04 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        String [] arr = br.readLine().split(" ");
+
+        int [] numArr = new int[n];
+
+        for(int i = 0; i < n; i++){
+            numArr[i] = Integer.parseInt(arr[i]);
+        }
+        Arrays.sort(numArr);
+
+        int result = 0;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < i + 1; j++){
+                result += numArr[j];
+            }
+        }
+        System.out.println(result);
+    }
+}
