afa3bb631c724b9dc055b81c1530d30d6940605b
lee <3bnm1128@naver.com>
2021-02-18 11:29:35 +0900

    백준, 그리디 - 동전 0

---
+++ src/com/algorithm/greedyEx05/GreedyEx05.java
@@ -0,0 +1,34 @@
+package com.algorithm.greedyEx05;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class GreedyEx05 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int kinds = Integer.parseInt(st.nextToken());
+        int amount = Integer.parseInt(st.nextToken());
+        int num [] = new int[kinds];
+
+        for(int i = 0; i < kinds; i++){
+            num[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(num);
+
+        int cnt = 0;
+        int tmp = amount;
+        for(int i = kinds - 1; i >= 0; i--){
+            if(tmp % num[i] < amount){
+                cnt += tmp / num[i];
+                tmp %= num[i];
+            }
+        }
+        System.out.println(cnt);
+    }
+}
