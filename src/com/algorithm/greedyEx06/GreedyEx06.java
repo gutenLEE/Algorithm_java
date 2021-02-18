3178e2bc72922f39a260e19eb11cb42b0a0dadfc
lee <3bnm1128@naver.com>
2021-02-18 14:21:32 +0900

    백준, 그리디 - 잃어버린 괄호(!)

--- src/com/algorithm/greedyEx06/GreedyEx06.java
+++ src/com/algorithm/greedyEx06/GreedyEx06.java
@@ -1,4 +1,33 @@
 package com.algorithm.greedyEx06;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class GreedyEx06 {
+
+    public static void main(String[]args) throws IOException {
+
+        int sum = Integer.MAX_VALUE;
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] subtraction = br.readLine().split("-");
+
+        for(int i = 0; i < subtraction.length; i++){
+            String [] add = subtraction[i].split("\\+");
+
+            int temp = 0;
+            for(int j = 0; j < add.length; j++){
+                temp += Integer.parseInt(add[j]);
+            }
+
+            if(sum == Integer.MAX_VALUE){
+                sum = temp;
+            }
+            else {
+                sum -= temp;
+            }
+        }
+        System.out.println(sum);
+    }
 }
