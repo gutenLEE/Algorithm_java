b13ca516c525af4415d6e381c98a5a1fb29d950a
lee <3bnm1128@naver.com>
2021-02-23 23:49:35 +0900

    이.코, 구현 - 시각 113p

--- src/com/algorithm/implementation/ImplEx02.java
+++ src/com/algorithm/implementation/ImplEx02.java
@@ -1,2 +1,32 @@
-package com.algorithm.implementation;public class ImplEx02 {
+package com.algorithm.implementation;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ImplEx02 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int hour = Integer.parseInt(br.readLine());
+        int count = 0;
+
+        for(int i = 0; i < hour + 1; i++){
+            for(int j = 0; j < 60; j++){
+                for(int k = 0; k < 60; k++){
+                    String h = Integer.toString(i);
+                    String m = Integer.toString(j);
+                    String s = Integer.toString(k);
+
+                    if((h+m+s).contains("3")){
+                        count += 1;
+                    }
+
+                }
+            }
+        }
+        System.out.println(count);
+    }
 }
