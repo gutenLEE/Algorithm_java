ce16d24baa1f916367a0ad31052c8f8d03f30961
lee <3bnm1128@naver.com>
2021-02-24 12:21:35 +0900

    이.코, 구현 - 왕실의 나이트

--- src/com/algorithm/implementation/ImplEx03.java
+++ src/com/algorithm/implementation/ImplEx03.java
@@ -1,7 +1,33 @@
 package com.algorithm.implementation;
 
-public class ImplEx03 {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class ImplEx03 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+        solution(input);
+    }
+    public static int solution(String input){
+        int row = Integer.parseInt(input.charAt(1) + "");
+        int col = input.charAt(0) - 97 + 1;
+
+        int [][] steps = {
+                {-2, -1}, {-1, -2}, {1, -2}, {2, -1},
+                {2, 1}, {1, 2}, {-1, 2}, {-2, 1}
+        };
+
+        int result = 0;
+        for(int[] step: steps) {
+            int nextR = row + step[0];
+            int nextC = col + step[1];
+
+            if (nextR >= 1 && nextR <= 8 && nextC >= 1 && nextC <= 8)
+                result++;
+        }
+        System.out.println(result);
+        return result;
     }
 }
