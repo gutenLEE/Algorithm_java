fe7d9e579f646bc9f02af2424cdda96a0c3694d3
lee <3bnm1128@naver.com>
2021-02-26 15:10:58 +0900

    생성자의 접근제어자 - 싱글톤

--- src/com/java/OOP/Singleton.java
+++ src/com/java/OOP/Singleton.java
@@ -2,8 +2,10 @@ package com.java.OOP;
 
 public class Singleton {
 
-    private static Singleton s = new Singleton();
-    private Singleton() { }
+    private static Singleton s = new Singleton(); // getInstance에서 사용될 수 있도록 인스턴스가 미리 생성.
+
+    private Singleton() { } 
+
     public static Singleton getInstance(){
         return s;
     }
