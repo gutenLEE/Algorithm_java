2f77f1e5512f86b62a02fb81fa82d2ca18534ffe
lee <3bnm1128@naver.com>
2021-03-01 17:34:41 +0900

    Random클래스로 난수 생성하기

--- src/com/java/Lang/ObjectsEx01.java
+++ src/com/java/Lang/ObjectsEx01.java
@@ -1,2 +1,12 @@
-package com.java.Lang;public class ObjectsEx01 {
+package com.java.Lang;
+
+import java.util.Objects;
+
+public class ObjectsEx01 {
+    public static void main(String[] args) {
+        String[][] str2D = new String[][]{{"A", "C"}, {"D", "E"}};
+        String[][] str2D2 = new String[][]{{"A", "C"}, {"D", "E"}};
+
+        System.out.println(Objects.deepEquals(str2D, str2D2));
+    }
 }
