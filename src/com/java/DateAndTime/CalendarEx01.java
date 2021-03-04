27a3e066bdfaf2ea92d39a32382e58f4e844d12e
lee <3bnm1128@naver.com>
2021-03-04 19:30:15 +0900

    자바의 정석 - Calendar

---
+++ src/com/java/DateAndTime/CalendarEx01.java
@@ -0,0 +1,31 @@
+package com.java.DateAndTime;
+
+import java.util.Calendar;
+
+public class CalendarEx01 {
+
+    public static void main(String[] args) {
+        final int[] TIME_UNIT = {3600, 60, 1};
+        final String[] TIME_UNIT_NAME = {"시간 ", "분 ", "초 "};
+
+        Calendar time1 = Calendar.getInstance();
+        Calendar time2 = Calendar.getInstance();
+
+        time1.set(Calendar.HOUR_OF_DAY, 10);
+        time1.set(Calendar.MINUTE, 23);
+        time1.set(Calendar.SECOND, 14);
+
+        time2.set(Calendar.HOUR_OF_DAY, 20);
+        time2.set(Calendar.MINUTE, 34);
+        time2.set(Calendar.SECOND, 55);
+
+        System.out.println("time1 - " + time1.get(Calendar.HOUR_OF_DAY) + ":" + time1.get(Calendar.MINUTE) + ":" + time1.get(Calendar.SECOND));
+        System.out.println("time2 - " + time2.get(Calendar.HOUR_OF_DAY) + ":" + time2.get(Calendar.MINUTE) + ":" + time2.get(Calendar.SECOND));
+
+        long difference = Math.abs(time2.getTimeInMillis() - time1.getTimeInMillis() / 1000);
+        System.out.println(difference);
+
+        Calendar today = Calendar.getInstance();
+        System.out.println(today.get(Calendar.YEAR));
+    }
+}
