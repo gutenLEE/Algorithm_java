8da85ed3c4cbc35ed36c2917cac14a03def44c57
lee <3bnm1128@naver.com>
2021-03-25 14:48:20 +0900

    지네릭 클래스와 상속관계

--- src/com/java/generic/Ex02Generic.java
+++ src/com/java/generic/Ex02Generic.java
@@ -1,2 +1,34 @@
-package com.java.generic;public class Ex02Generic {
+package com.java.generic;
+
+import java.util.ArrayList;
+
+public class Ex02Generic {
+    public static void main(String[] args) {
+        Box2<Fruit> fruitBox = new Box2<>();
+        Box2<Apple> appleBox2 = new Box2<>();
+        Box2<Toy> toyBox2 = new Box2<>();
+        //Box2<Grape> grapeBox2 = new Box2<Apple>();
+
+        fruitBox.add(new Fruit());
+        fruitBox.add(new Apple());
+        appleBox2.add(new Apple());
+        toyBox2.add(new Toy());
+
+        System.out.println(fruitBox);
+        System.out.println(appleBox2);
+        System.out.println(toyBox2);
+    }
+}
+
+class Fruit{public String toString() {return "Fruit";}}
+class Apple extends Fruit{public String toString() {return "Apple";}}
+class Grape{public String toString() {return "Grape";}}
+class Toy{public String toString() {return "Toy";}}
+
+class Box2<T> {
+    ArrayList<T> list = new ArrayList<T>();
+    void add(T item){ list.add(item);}
+    T get(int i){return list.get(i);}
+    int size(){return list.size();}
+    public String toString(){return list.toString();}
 }
