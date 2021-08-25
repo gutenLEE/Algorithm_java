112a58aabdfd867aa4b40cfbc8188570b0fea437
gutenLEE <3bnm1128@naver.com>
2021-08-25 21:19:51 +0900

    수정중....

--- src/com/algorithm/datastructure/LinkedList_01.java
+++ src/com/algorithm/datastructure/LinkedList_01.java
@@ -1,21 +1,36 @@
 package com.algorithm.datastructure;
 
+import lombok.Setter;
+import lombok.ToString;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+
 /**
  * 백준 5397번, 키로커
  * @author yhlee
  * @created_at 2021-08-24 오후 10:16
  */
 public class LinkedList_01 {
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws IOException {
         // 입력
         // <, > : insert
         // - : delete
-        
+
+        DoublyLinkedList a = new DoublyLinkedList("A");
+        DoublyLinkedList b = new DoublyLinkedList("B");
+        System.out.println(a.toString());
+        System.out.println(b.toString());
 
     }
 }
 
+@Setter
 class Node{
+
     private Object data;
     private Node next;
     private Node prev;
@@ -26,13 +41,30 @@ class Node{
         this.prev = null;
     }
 
+    public Node(Object input, Node prev) {
+        this.data = input;
+        this.prev = prev;
+    }
+
+    public Node(Object input, Node prev, Node next) {
+        this.data = input;
+        this.prev = prev;
+        this.next =  next;
+    }
+
     public String toString() {
         return String.valueOf(this.data);
     }
 }
 
+@Setter @ToString
 class DoublyLinkedList{
     private Node head;
     private Node tail;
     private int size = 0;
+
+    public DoublyLinkedList(Object data) {
+        this.head = new Node(data);
+        this.tail = this.head;
+    }
 }
